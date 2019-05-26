a2b2e7e99c132fd924188be13a490ec7ec67aeb5
ErikasIlk <49658485+ErikasIlk@users.noreply.github.com>
2019-05-26 20:53:03 +0300

    Add files via upload

---
+++ form/public/formAfter.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<title>Parašykite mums!</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" rel="stylesheet">
+	<link href="../../css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="topNav" id="topNavBar">
+			<a href="../../index.php">Pagrindinis</a> <a href="../../aboutUs.php">Apie mus</a> <a href="form.php">Parašykite mums!</a> <a class="topNavIcon" href="javascript:void(0);" onclick="openMobileNavbar()"><i class="fas fa-brain"></i></a>
+		</div>
+		<div id="formAfter">
+			<h2>Ačiū, Jūsų žinutė gauta!</h2>
+		</div>
+		<div class="row">
+			<footer id="formAfterFooter">
+				<p>&copy;ErikasIlk<?php echo date('Y')?></p>
+			</footer>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
+	</script> 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
+	</script> 
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+	</script> 
+	<script src="../../script.html" type="text/javascript">
+	</script>
+</body>
+</html>
